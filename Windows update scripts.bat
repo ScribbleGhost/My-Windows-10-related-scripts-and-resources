6c01e6f033b5c9c899e7adad59acec85ae5337fb
ScribbleG <49068170+ScribbleGhost@users.noreply.github.com>
2019-04-07 23:18:43 +0200

    Update Windows update scripts.bat

--- Windows update scripts.bat
+++ Windows update scripts.bat
@@ -9,7 +9,7 @@ REM ### Windows Update
 		REM LINK: http://www.tenforums.com/tutorials/8013-windows-update-automatic-updates-enable-disable-windows-10-a.html
 			REM OPTIONS: 0x00000001=On, 0x00000000=Off
 				REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /F 1>NUL
-				REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /V "NoAutoUpdate" /T "REG_DWORD" /D "0x00000001" /F 1>NUL	REM TITLE: Never notify or check for Windows updates
+				REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /V "NoAutoUpdate" /T "REG_DWORD" /D "0x00000001" /F 1>NUL
 
 	REM TITLE: Notify before downloading and installing Windows updates
 		REM LINK: http://www.tenforums.com/tutorials/8013-windows-update-automatic-updates-enable-disable-windows-10-a.html
